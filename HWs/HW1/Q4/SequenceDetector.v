cf1032b84fd25f2dfb29b20b7367bfd0328a6942
mm.mehran79 <mm.mehran79@gmail.com>
2022-11-20 06:11:37 +0330

    Q4 done

--- HWs/HW1/Q4/SequenceDetector.v
+++ HWs/HW1/Q4/SequenceDetector.v
@@ -1,1 +1,27 @@
-fd
+//===========================================================
+//
+//			Sequence Detector
+//
+//			Implemented by Mehran Mazaheri
+//
+//          SEQUENCE is parameterized 
+//          
+//===========================================================
+`timescale 1ns/1ns
+module SequenceDetector
+#(
+    parameter SEQUENCE_LENGTH = 11,
+    parameter reg [SEQUENCE_LENGTH-1:0]SEQUENCE_PATTERN = SEQUENCE_LENGTH'b01011101000;
+)(
+    input wire clk,
+    input wire in,
+    output wire Sequence_detected
+)
+    reg [SEQUENCE_LENGTH-1:0] sequence = SEQUENCE_LENGTH'b0;
+//----------------------------------------------- Sequential Logic
+    always @(posedge clk) begin
+        sequence <= {in,sequence[SEQUENCE_LENGTH-1:1]};     //Shift Register
+    end
+//----------------------------------------------- Continuous Assignment
+    assign Sequence_detected = SEQUENCE_PATTERN == sequence;
+endmodule
